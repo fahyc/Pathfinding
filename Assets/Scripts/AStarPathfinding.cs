873fdf935c0517747b664d2c137c0e3bace823cd
John Steigerwald <johnstiggy@gmail.com>
2016-10-24 22:34:44 -0400

    A* Pathfinding seems to work

---
+++ Assets/Scripts/AStarPathfinding.cs
@@ -0,0 +1,161 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AStarPathfinding : MonoBehaviour {
+
+    MapGeneration mapInfo;
+    int xPos;
+    int yPos;
+    public int currentHeuristic;
+
+    MapGeneration.Node target;
+    List<MapGeneration.Node> pathToUse;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Get the map data from the Map Generation Script
+        mapInfo = Camera.main.GetComponent<MapGeneration>();
+
+        //Random start location for the actor
+        do
+        {
+            xPos = (int) Mathf.Floor(mapInfo.mapData[0].Count * Random.value);
+            yPos = (int) Mathf.Floor(mapInfo.mapData.Count * Random.value);
+        } while (!mapInfo.mapData[yPos][xPos].passable);
+
+        xPos = 15;
+        yPos = 5;
+
+        //Set the player at the center of the square
+        transform.position = new Vector3(mapInfo.mapData[yPos][xPos].xPos,
+                                         mapInfo.mapData[yPos][xPos].yPos, -15);
+
+        //Generate a node for the target
+        target = new MapGeneration.Node(0, 0, true);
+
+        //Random location for the target
+        do
+        {
+            target.xLoc = (int)Mathf.Floor(mapInfo.mapData[0].Count * Random.value);
+            target.yLoc = (int)Mathf.Floor(mapInfo.mapData.Count * Random.value);
+        } while (!mapInfo.mapData[target.yLoc][target.xLoc].passable);
+
+        target.xLoc = 15;
+        target.yLoc = 19;
+        //Place the destination on the map
+        Instantiate(mapInfo.destination, new Vector3(mapInfo.mapData[target.yLoc][target.xLoc].xPos, mapInfo.mapData[target.yLoc][target.xLoc].yPos, -15f), Quaternion.identity);
+
+        Camera.main.transform.parent = this.transform;
+        Camera.main.transform.localPosition = new Vector3(0, 0, -25);
+
+        MapGeneration.Node destNode = FindPath(target.xLoc, target.yLoc);
+        pathToUse = new List<MapGeneration.Node>();
+        while (destNode != null)
+        {
+            pathToUse.Add(destNode);
+            destNode = destNode.parent;
+        }
+        pathToUse.Reverse();
+    }
+
+    void Update()
+    {
+
+    }
+
+    //Check the four adjacent neighborsand add them to a temproary list
+    List<MapGeneration.Node> findNeighbors(MapGeneration.Node current)
+    {
+        List<MapGeneration.Node> neighbors = new List<MapGeneration.Node>();
+        if (current.xLoc != 0 && mapInfo.mapData[current.yLoc][current.xLoc - 1].passable)
+        {
+            neighbors.Add(mapInfo.mapData[current.yLoc][current.xLoc - 1]);
+        }
+        if(current.xLoc != mapInfo.mapData[0].Count-1&& mapInfo.mapData[current.yLoc][current.xLoc + 1].passable)
+        {
+            neighbors.Add(mapInfo.mapData[current.yLoc][current.xLoc + 1]);
+        }
+        if(current.yLoc != 0 && mapInfo.mapData[current.yLoc -1][current.xLoc ].passable)
+        {
+            neighbors.Add(mapInfo.mapData[current.yLoc - 1][current.xLoc]);
+        }
+        if(current.yLoc != mapInfo.mapData.Count - 1 && mapInfo.mapData[current.yLoc + 1][current.xLoc].passable)
+        {
+            neighbors.Add(mapInfo.mapData[current.yLoc + 1][current.xLoc]);
+        }
+        return neighbors;
+    }
+
+    //Determines which heuristic to use
+    float useHeuristic(MapGeneration.Node current)
+    {
+        switch (currentHeuristic)
+        {
+            case 1:
+                return Mathf.Abs(current.xLoc - target.xPos) + Mathf.Abs(current.yLoc - target.yPos);
+            default:
+                return Vector2.Distance(new Vector2(current.xLoc, current.yLoc), new Vector2(target.xLoc, target.yLoc));
+        }
+
+    }
+
+    MapGeneration.Node FindPath(int xDest, int yDest)
+    {
+        List<MapGeneration.Node> open = new List<MapGeneration.Node>();
+        List<MapGeneration.Node> closed = new List<MapGeneration.Node>();
+        open.Add(mapInfo.mapData[this.yPos][this.xPos]);
+        while(open.Count > 0)
+        {
+            MapGeneration.Node current = open[0];
+            open.RemoveAt(0);
+            if(current.xLoc == xDest && current.yLoc == yDest)
+            {
+                return current;
+            }
+            else
+            {
+                closed.Add(current);
+
+                foreach (MapGeneration.Node neighbor in findNeighbors(current))
+                {
+                    bool inClosed = closed.Contains(neighbor);
+                    bool inOpen = open.Contains(neighbor);
+
+                    if (inClosed && current.g + 1 < neighbor.g)
+                    {
+                        Debug.Log("Closed");
+                        neighbor.g = current.g+1;
+                        neighbor.parent = current;
+
+                    }
+                    else if (inOpen && current.g + 1 < neighbor.g)
+                    {
+                        Debug.Log("open");
+                        neighbor.g = current.g+1;
+                        neighbor.parent = current;
+                    }
+                    else if(!inOpen && !inClosed)
+                    {
+                        neighbor.h = useHeuristic(neighbor);
+                        neighbor.g = neighbor.g + 1;
+                        neighbor.parent = current;
+                       
+                        int index = open.BinarySearch(neighbor);
+                        if(index < 0)
+                        {
+                            open.Insert(~index, neighbor); //Add at the theoretical location
+                        }
+                        else
+                        {
+                            open.Insert(index, neighbor);
+                        }
+                        
+                    }
+                }
+            }
+        }
+        return new MapGeneration.Node(this.xPos, this.yPos, true);
+    }
+}
--- Assets/Scripts/MapGeneration.cs
+++ Assets/Scripts/MapGeneration.cs
@@ -3,28 +3,56 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System;
+using UnityEditor;
 
 public class MapGeneration : MonoBehaviour {
 
     private GameObject map;
     public string mapName;
+
+    public GameObject player;
+    public GameObject destination;
     public GameObject treeTile;
     public GameObject floorTile;
     public GameObject passableTile;
     [HideInInspector]
     public List<List<Node>> mapData;
 
-    public class Node
+    public class Node : IComparable<Node>
     {
+        public Node parent;
+        //Location for character movement, is the center of the tile
         public float xPos;
         public float yPos;
+
+        //Location in the Graph for pathfinding
+        public int xLoc;
+        public int yLoc;
+        public float g = 0; // Cost to this point
+        public float h = 0; // Heuristic cost
         public bool passable;
 
-        public Node(float xL, float yL, bool p)
+        public Node( int xL, int yL, bool p)
         {
-            xPos = xL ;
-            yPos = yL;
+            xPos = xL + .5f;
+            yPos = -yL - .5f;
+            xLoc = xL/2;
+            yLoc = yL/2;
             passable = p;
+            parent = null;
+        }
+
+        int IComparable<Node>.CompareTo(Node a)
+        {
+            Node n1 = this;
+            Node n2 = a;
+            if (n1.g + n1.h > n2.g + n2.h)
+                return 1;
+            if (n1.g + n1.h < n2.g + n2.h)
+                return -1;
+            else
+                return 0;
         }
     }
 
@@ -33,7 +61,8 @@ public class MapGeneration : MonoBehaviour {
         map = new GameObject();
         mapData = new List<List<Node>>();
         Load();
-        genData();
+        GenerateData();
+        Instantiate(player, Vector3.zero, Quaternion.identity);
     }
 
     private bool Load()
@@ -71,7 +100,8 @@ public class MapGeneration : MonoBehaviour {
         }
     }
 
-    private void genData()
+    //Generate the movement tiles
+    private void GenerateData()
     {            
         string[] mapInfo = new StreamReader(Application.dataPath + "/" + mapName).ReadToEnd().Split('\n');
         for (int y = 0; y < mapInfo.Length-1; y += 2)
@@ -81,11 +111,11 @@ public class MapGeneration : MonoBehaviour {
             {
                 if(mapInfo[y][x] != '.' || mapInfo[y][x+1] != '.' || mapInfo[y+1][x] != '.' || mapInfo[y+1][x+1] != '.')
                 {
-                    mapData[y / 2].Add(new Node(y, x, false));
+                    mapData[y / 2].Add(new Node(x, y, false));
                 }
                 else
                 {
-                    mapData[y / 2].Add(new Node(x + .5f, -y - .5f, true));
+                    mapData[y / 2].Add(new Node(x, y, true));
                     Instantiate(passableTile, new Vector3(x+.5f, -y - .5f, -1), Quaternion.identity, map.transform);
                 }
             }
